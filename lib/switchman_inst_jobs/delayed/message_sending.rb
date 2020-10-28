aa42a8c3647d53c62d5f669d399fadd591f57b6b
Cody Cutrer <cody@instructure.com>
2020-10-28 16:21:13 -0600

    capture the proper sender for wrapped delay method
    Change-Id: I2e358233acf7a0284be5524df83c2ad13ebb57af
    Reviewed-on: https://gerrit.instructure.com/c/switchman-inst-jobs/+/251386
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- lib/switchman_inst_jobs/delayed/message_sending.rb
+++ lib/switchman_inst_jobs/delayed/message_sending.rb
@@ -1,8 +1,9 @@
 module SwitchmanInstJobs
   module Delayed
     module MessageSending
-      def delay(**enqueue_args)
-        return self if ::Switchman::DatabaseServer.creating_new_shard
+      def delay(public_send: nil, synchronous: false, **enqueue_args)
+        public_send ||= __calculate_public_send_for_delay
+        synchronous ||= ::Switchman::DatabaseServer.creating_new_shard
 
         super
       end
