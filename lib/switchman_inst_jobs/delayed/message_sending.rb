48571807504a463e9017fa7f02adae8171acbd47
Cody Cutrer <cody@instructure.com>
2020-10-29 16:04:36 -0600

    pass along the proper argument to delay
    Change-Id: I32b1157ba24244632b6134ef309eeed0d58abd79
    Reviewed-on: https://gerrit.instructure.com/c/switchman-inst-jobs/+/251506
    Reviewed-by: Keith Garner <kgarner@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- lib/switchman_inst_jobs/delayed/message_sending.rb
+++ lib/switchman_inst_jobs/delayed/message_sending.rb
@@ -1,8 +1,8 @@
 module SwitchmanInstJobs
   module Delayed
     module MessageSending
-      def delay(public_send: nil, synchronous: false, **enqueue_args)
-        public_send ||= __calculate_public_send_for_delay
+      def delay(sender: nil, synchronous: false, **enqueue_args)
+        sender ||= __calculate_sender_for_delay
         synchronous ||= ::Switchman::DatabaseServer.creating_new_shard
 
         super
