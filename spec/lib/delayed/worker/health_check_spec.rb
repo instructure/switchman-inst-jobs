8b94194657a6e70c7cbfa9ef07b94f970ba620ba
Cody Cutrer <cody@instructure.com>
2021-09-28 11:59:06 -0600

    bump inst-jobs
    and import and tweak the new migration
    
    Change-Id: I49401f7947c6847ea4b56565dc40007cea227155
    Reviewed-on: https://gerrit.instructure.com/c/switchman-inst-jobs/+/274673
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Ethan Vizitei <evizitei@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    Reviewed-on: https://gerrit.instructure.com/c/switchman-inst-jobs/+/277212
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Reviewed-by: August Thornton <august@instructure.com>
    QA-Review: Jacob Burroughs <jburroughs@instructure.com>
    Product-Review: Jacob Burroughs <jburroughs@instructure.com>

--- spec/lib/delayed/worker/health_check_spec.rb
+++ spec/lib/delayed/worker/health_check_spec.rb
@@ -4,14 +4,14 @@ describe SwitchmanInstJobs::Delayed::Worker::HealthCheck do
 
     it 'cleans jobs on each shard, but files no jobs' do
       @shard1.update_attribute(:delayed_jobs_shard_id, @shard1.id)
-      @shard2.update_attribute(:delayed_jobs_shard_id, @shard2.id)
+      ::Switchman::Shard.default.update_attribute(:delayed_jobs_shard_id, ::Switchman::Shard.default.id)
 
       allow(::SwitchmanInstJobs::Delayed::Settings).to receive(:configured_shard_ids).
-        and_return([@shard1.id, @shard2.id])
+        and_return([@shard1.id, ::Switchman::Shard.default.id])
       expect(Delayed::Settings).to receive(:worker_health_check_type).at_least(:once).and_return(:consul)
       health_checker = double(live_workers: [])
       allow(::Delayed::Worker::HealthCheck).to receive(:build).and_return(health_checker)
-      expect(Delayed::Job).to receive(:running_jobs).exactly(2).times.and_return([])
+      expect(Delayed::Job).to receive(:running_jobs).exactly(2).times.and_return(Delayed::Job.none)
       expect(Delayed::Worker::HealthCheck).to receive(:delay).never
       # these two shards share a database, and the test transaction will prevent the lock from being
       # release between shards. so just don't get one
