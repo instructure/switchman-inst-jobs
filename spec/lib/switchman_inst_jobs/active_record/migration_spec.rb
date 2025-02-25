30dcb1463581500b736afb3c84c050b69a6384af
Aaron Ogata <aogata@instructure.com>
2025-02-25 10:20:01 -0800

    add support for rails 7.2

--- spec/lib/switchman_inst_jobs/active_record/migration_spec.rb
+++ spec/lib/switchman_inst_jobs/active_record/migration_spec.rb
@@ -4,27 +4,29 @@ describe SwitchmanInstJobs::ActiveRecord::Migration do
   it "Has a copy of all delayed_engine migrations" do
     delayed_engine = Rails.application.railties.detect { |rt| rt.railtie_name == "delayed_engine" }
     delayed_migrations_path = delayed_engine.paths["db/migrate"].first
-    delayed_migrations = if Rails.version >= "6"
+    delayed_migrations = if Rails.version < "7.2"
                            ActiveRecord::MigrationContext.new(
                              delayed_migrations_path,
                              ActiveRecord::SchemaMigration
                            ).migrations
                          else
                            ActiveRecord::MigrationContext.new(
-                             delayed_migrations_path
+                             delayed_migrations_path,
+                             ActiveRecord::SchemaMigration.new(ActiveRecord::Base.connection.pool)
                            ).migrations
                          end
 
     switchman_inst_jobs_engine = Rails.application.railties.detect { |rt| rt.railtie_name == "switchman_inst_jobs" }
     switchman_inst_jobs_migrations_path = switchman_inst_jobs_engine.paths["db/migrate"].first
-    switchman_inst_jobs_migrations = if Rails.version >= "6"
+    switchman_inst_jobs_migrations = if Rails.version < "7.2"
                                        ActiveRecord::MigrationContext.new(
                                          switchman_inst_jobs_migrations_path,
                                          ActiveRecord::SchemaMigration
                                        ).migrations
                                      else
                                        ActiveRecord::MigrationContext.new(
-                                         switchman_inst_jobs_migrations_path
+                                         switchman_inst_jobs_migrations_path,
+                                         ActiveRecord::SchemaMigration.new(ActiveRecord::Base.connection.pool)
                                        ).migrations
                                      end
 
